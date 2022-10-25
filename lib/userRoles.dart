1731293962f08832171d09177fdfad97cf7c83a1
neo4508 <zombiequeen6@gmail.com>
2022-10-25 22:11:19 +0700

    add userRoles

--- lib/userRoles.dart
+++ lib/userRoles.dart
@@ -2,7 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 String adminRole = "system administrator";
 String user = "user";
-String Role = "Role";
+String role = "Role";
+String userEmail = "Email";
 
 void changeUserRole (String selectedRole, String selectedEmail) {
 
@@ -11,10 +12,12 @@ void changeUserRole (String selectedRole, String selectedEmail) {
 void handleSubmitChangeRole (String currentEmail, String userEmail, String userRoleChanged) {
   String currentRole = "";
   QuerySnapshot querySnapshot = getUserData(currentEmail).then((results)=> results) as QuerySnapshot<Object>;
-  currentRole = querySnapshot.docs[0].get(Role);
+  currentRole = querySnapshot.docs[0].get(role);
   if (currentRole != adminRole) return;
   // Update user by searching emails
-  changeUserRole(userRoleChanged, userEmail);
+  if (userEmail == querySnapshot.docs[0].get(userEmail) && currentRole == adminRole) {
+    changeUserRole(userRoleChanged, userEmail);
+  }
 }
 
 Future getUserData (String userEmail)async {
